78cc8ff28f9ebd1939eed067e25999c77b73837c
Esakor <100299697+Esakor@users.noreply.github.com>
2022-10-19 14:42:35 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,13 +3,24 @@ function Turn_Right () {
     pins.servoSetPulse(AnalogPin.P13, 1700)
     control.waitMicros(20000)
 }
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 1) {
+        Forward()
+    } else if (receivedNumber == 2) {
+        Backward()
+    } else if (receivedNumber == 3) {
+        Turn_Right()
+    } else if (receivedNumber == 4) {
+        Turn_Left()
+    }
+})
 function Forward () {
     pins.servoSetPulse(AnalogPin.P8, 1300)
     pins.servoSetPulse(AnalogPin.P13, 1700)
     control.waitMicros(20000)
 }
 input.onButtonPressed(Button.A, function () {
-    Forward()
+	
 })
 function Turn_Left () {
     pins.servoSetPulse(AnalogPin.P8, 1300)
@@ -17,13 +28,13 @@ function Turn_Left () {
     control.waitMicros(20000)
 }
 input.onButtonPressed(Button.AB, function () {
-    Turn_Right()
+	
 })
 input.onButtonPressed(Button.B, function () {
-    Backward()
+	
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    Turn_Left()
+	
 })
 function sensor () {
     pins.digitalWritePin(DigitalPin.P1, 0)
@@ -32,9 +43,10 @@ function sensor () {
     control.waitMicros(10)
     pins.digitalWritePin(DigitalPin.P1, 0)
     distance = pins.pulseIn(DigitalPin.P2, PulseValue.High) / 58
-    if (distance < 8) {
-        pins.servoSetPulse(AnalogPin.P8, 0)
-        pins.servoSetPulse(AnalogPin.P13, 0)
+    if (distance < 30) {
+        Turn_Right()
+        control.waitMicros(20000)
+        Forward()
         control.waitMicros(20000)
     }
 }
@@ -44,8 +56,9 @@ function Backward () {
     control.waitMicros(20000)
 }
 let distance = 0
-basic.showIcon(IconNames.TShirt)
+basic.showIcon(IconNames.Rollerskate)
 distance = 0
+radio.setGroup(7)
 basic.forever(function () {
     sensor()
     basic.showNumber(distance)
