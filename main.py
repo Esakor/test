f1f15b4c10844bc0209edcd507285bc2c51d1adf
Esakor <100299697+Esakor@users.noreply.github.com>
2022-10-20 14:41:29 -0400

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,33 +1,26 @@
-def Turn_Right():
-    pins.servo_set_pulse(AnalogPin.P8, 0)
-    pins.servo_set_pulse(AnalogPin.P13, 1700)
-    control.wait_micros(20000)
+def on_received_number(receivedNumber):
+    if receivedNumber == 1:
+        Forward()
+    elif receivedNumber == 2:
+        Backward()
+    elif receivedNumber == 3:
+        Turn_Right()
+    elif receivedNumber == 4:
+        Turn_Left()
+radio.on_received_number(on_received_number)
+
 def Forward():
     pins.servo_set_pulse(AnalogPin.P8, 1300)
     pins.servo_set_pulse(AnalogPin.P13, 1700)
     control.wait_micros(20000)
-
-def on_button_pressed_a():
-    Forward()
-input.on_button_pressed(Button.A, on_button_pressed_a)
-
+def Turn_Right():
+    pins.servo_set_pulse(AnalogPin.P8, 0)
+    pins.servo_set_pulse(AnalogPin.P13, 1700)
+    control.wait_micros(20000)
 def Turn_Left():
     pins.servo_set_pulse(AnalogPin.P8, 1300)
     pins.servo_set_pulse(AnalogPin.P13, 0)
     control.wait_micros(20000)
-
-def on_button_pressed_ab():
-    Turn_Right()
-input.on_button_pressed(Button.AB, on_button_pressed_ab)
-
-def on_button_pressed_b():
-    Backward()
-input.on_button_pressed(Button.B, on_button_pressed_b)
-
-def on_logo_pressed():
-    Turn_Left()
-input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_pressed)
-
 def sensor():
     global distance
     pins.digital_write_pin(DigitalPin.P1, 0)
@@ -36,19 +29,25 @@ def sensor():
     control.wait_micros(10)
     pins.digital_write_pin(DigitalPin.P1, 0)
     distance = pins.pulse_in(DigitalPin.P2, PulseValue.HIGH) / 58
-    if distance < 8:
-        pins.servo_set_pulse(AnalogPin.P8, 0)
-        pins.servo_set_pulse(AnalogPin.P13, 0)
-        control.wait_micros(20000)
 def Backward():
     pins.servo_set_pulse(AnalogPin.P8, 1700)
     pins.servo_set_pulse(AnalogPin.P13, 1300)
     control.wait_micros(20000)
+X = 0
 distance = 0
-basic.show_icon(IconNames.TSHIRT)
+basic.show_icon(IconNames.FABULOUS)
 distance = 0
+radio.set_group(7)
 
 def on_forever():
-    sensor()
-    basic.show_number(distance)
+    global X
+    X = 0
+    for index in range(4):
+        sensor()
+        if distance <= 15:
+            X += 1
+    if X == 4:
+        Turn_Right()
+        basic.pause(100)
+        Forward()
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,8 +1,3 @@
-function Turn_Right () {
-    pins.servoSetPulse(AnalogPin.P8, 0)
-    pins.servoSetPulse(AnalogPin.P13, 1700)
-    control.waitMicros(20000)
-}
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
         Forward()
@@ -19,23 +14,16 @@ function Forward () {
     pins.servoSetPulse(AnalogPin.P13, 1700)
     control.waitMicros(20000)
 }
-input.onButtonPressed(Button.A, function () {
-	
-})
+function Turn_Right () {
+    pins.servoSetPulse(AnalogPin.P8, 0)
+    pins.servoSetPulse(AnalogPin.P13, 1700)
+    control.waitMicros(20000)
+}
 function Turn_Left () {
     pins.servoSetPulse(AnalogPin.P8, 1300)
     pins.servoSetPulse(AnalogPin.P13, 0)
     control.waitMicros(20000)
 }
-input.onButtonPressed(Button.AB, function () {
-	
-})
-input.onButtonPressed(Button.B, function () {
-	
-})
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-	
-})
 function sensor () {
     pins.digitalWritePin(DigitalPin.P1, 0)
     control.waitMicros(2)
@@ -43,23 +31,28 @@ function sensor () {
     control.waitMicros(10)
     pins.digitalWritePin(DigitalPin.P1, 0)
     distance = pins.pulseIn(DigitalPin.P2, PulseValue.High) / 58
-    if (distance < 30) {
-        Turn_Right()
-        control.waitMicros(20000)
-        Forward()
-        control.waitMicros(20000)
-    }
 }
 function Backward () {
     pins.servoSetPulse(AnalogPin.P8, 1700)
     pins.servoSetPulse(AnalogPin.P13, 1300)
     control.waitMicros(20000)
 }
+let X = 0
 let distance = 0
-basic.showIcon(IconNames.Rollerskate)
+basic.showIcon(IconNames.Fabulous)
 distance = 0
 radio.setGroup(7)
 basic.forever(function () {
-    sensor()
-    basic.showNumber(distance)
+    X = 0
+    for (let index = 0; index < 4; index++) {
+        sensor()
+        if (distance <= 15) {
+            X += 1
+        }
+    }
+    if (X == 4) {
+        Turn_Right()
+        basic.pause(100)
+        Forward()
+    }
 })
